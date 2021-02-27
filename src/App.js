00caebb8b93e08facb1571f89b6eeb43833343d9
Guillx <guillx.gv@gmail.com>
2021-02-27 21:55:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link, Route } from "wouter";
+import ListOfGifs from "./components/ListOfGifs";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <h1>Escoge una categoría:</h1>
+        <div className="links">
+          <Link to="/gif/ardilla">Gifs de Ardillas</Link>
+          <Link to="/gif/buho">Gifs de Buhos</Link>
+          <Link to="/gif/caballo">Gifs de Caballos</Link>
+          <Link to="/gif/delfin">Gifs de Delfines</Link>
+          <Link to="/gif/elefante">Gifs de Elefantes</Link>
+          <Link to="/gif/flamenco">Gifs de Flamencos</Link>
+          <Link to="/gif/gato">Gifs de Gatos</Link>
+          <Link to="/gif/hipopotamo">Gifs de Hipopótamos</Link>
+          <Link to="/gif/iguana">Gifs de Iguanas</Link>
+          <Link to="/gif/jirafa">Gifs de Jirafas</Link>
+          <Link to="/gif/koala">Gifs de Koalas</Link>
+          <Link to="/gif/leon">Gifs de Leones</Link>
+          <Link to="/gif/mapache">Gifs de Mapaches</Link>
+          <Link to="/gif/nutria">Gifs de Nutrias</Link>
+          <Link to="/gif/ñu">Gifs de Ñus</Link>
+          <Link to="/gif/oso">Gifs de Osos</Link>
+          <Link to="/gif/perro">Gifs de Perros</Link>
+          <Link to="/gif/rinoceronte">Gifs de Rinocerontes</Link>
+          <Link to="/gif/serpiente">Gifs de Serpientes</Link>
+          <Link to="/gif/tiburon">Gifs de Tiburones</Link>
+          <Link to="/gif/vaca">Gifs de Vacas</Link>
+          <Link to="/gif/zorro">Gifs de Zorros</Link>
+        </div>
+        <Route component={ListOfGifs} path="/gif/:keyword" />
+      </section>
     </div>
   );
 }
---
+++ src/components/ListOfGifs.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import Gif from "./Gif";
+import getGifs from "../services/GetGifs";
+
+export default function ListOfGifs({ params }) {
+  const { keyword } = params;
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(
+    function () {
+      getGifs({ keyword }).then((gifs) => setGifs(gifs));
+    },
+    [keyword]
+  );
+
+  return (
+    <>
+      {gifs.map(({ id, title, url }) => (
+        <Gif key={id} id={id} title={title} url={url} />
+      ))}
+    </>
+  );
+}
